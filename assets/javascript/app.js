30b8e62af7a95298265054100072e27869c71398
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-25 22:54:12 -0400

    a good start

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//variables
+var userChoice;
+
+var correctAnswer = 0;
+
+var incorrectAnswer = 0;
+
+var images;
+
+var questions = [{
+
+    question:
+    answer:
+
+
+}]
+
+
+//arry of objects -- need questions & need answers w/ choices
+
+
+//need timer
+

29bd4fb3c708444966a1189c2e1a2b63e22d8e38
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-31 13:34:11 -0400

    picasso theme

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -3,135 +3,117 @@ $(document).ready(function () {
     //const results = $("<div>");
     //const submit = $("<button>");
 
-    $(function(){
+    $(function () {
         const qaChoice = [{
-            question: "What is the biggest fish in the ocean?",
-            answer: ["basking shark", "whale shark", "blue whale", "jaws"],
-            images: "assets/baskingshark.jpg",
-            correctAnswer: "basking shark"
-        },
+                question: "What is Picasso's last name at birth?",
+                answer: ["Gonzalez", "Ruiz", "Mendoza", "Valdez"],
+                images: "#",
+                correctAnswer: "Ruiz"
+            },
 
-        {
-            question: "What was the original color of Oscar the Grouch?",
-            answer: ["orange", "purple", "green", "pink"],
-            images: "assets/orangeoscar.jpg",
-            correctAnswer: "orange"
-        },
+            {
+                question: "What famous painting was Picasso accured of stealing in 1911?",
+                answer: ["Mona Lisa", "Water Lillies", "Venus de Milo", "Last Supper"],
+                images: "#",
+                correctAnswer: "Mona Lisa"
+            },
 
-        {
-            question: "what does 'M & M's' actually stand for?",
-            answer: ["Mmmmm & Mmmmmmm", "marvelous & ", "Mork & Mindy", "Mars & Murie's"],
-            images: "assets/mork&mindy.jpg",
-            correctAnswer: "Mork & Mindy"
-        },
+            {
+                question: "How old was Picasso when he died?",
+                answer: ["23", "42", "70", "91"],
+                images: "#",
+                correctAnswer: "91"
+            },
 
-        {
-            question: "The skin of which fruit can cause poison ivy symptoms?",
-            answer: ["kiwi", "mango", "pomegranate", "papaya"],
-            images: "assets/kiwi.jpg",
-            correctAnswer: "kiwi"
-        },
+            {
+                question: "How long did Picasso take to paint Guernica?",
+                answer: ["3 months", "3 years", "3 weeks", "3 days"],
+                images: "#",
+                correctAnswer: "3 weeks"
+            },
 
-        {
-            question: "Which president owned a bar before taking office?",
-            answer: ["Millard Fillmore", "Lyndon B. Johnson", "Jimmy Carter", "Abraham Lincoln"],
-            images: "assets/abrahamlincoln.jpg",
-            correctAnswer: "Abraham Lincoln"
-        },
+            {
+                question: "Which painting led to the breakup of Picasso's marriage?",
+                answer: ["La Lecture", "Olga in the Armchair", "Les Demoiselles", "The Old Guitarist"],
+                images: "#",
+                correctAnswer: "La Lecture"
+            },
 
-        {
-            question: "Play-doh was originally made to be...",
-            answer: ["stress reliever", "wall-paper remover", "glue", "salt-lick"],
-            images: "assets/play-doh.jpg",
-            correctAnswer: "wall-paper remover"
-        },
+            {
+                question: "In what country did Picasso die?",
+                answer: ["USA", "France", "China", "Spain"],
+                images: "#",
+                correctAnswer: "France"
+            },
 
-        {
-            question: "A baby puffin is called a ...",
-            answer: ["puff puff", "little puffer", "puffling", "puffy"],
-            images: "assets/puffling.jpg",
-            correctAnswer: "puffling"
-        },
+            {
+                question: "Which painter did Picasso admire as a master?",
+                answer: ["Munch", "lMatisse", "Cezanne", "O'Keefe"],
+                images: "#",
+                correctAnswer: "Cezanne"
+            },
 
-        {
-            question: "A group of bunnies is called a ...",
-            answer: ["kerfluffle", "fluffle", "hoppy", "fluffy hoppy"],
-            images: "assets/bunnies.jpg",
-            correctAnswer: "fluffle"
-        },
+        ];
+        //display questions and answers
+        qaChoice.sort(function () {
+            return 0.5 - Math.random();
+        });
 
-        {
-            question: "How many ridges are on a dime?",
-            answer: ["58", "202", "118", "30"],
-            images: "assets/dime.jpg",
-            correctAnswer: "118"
-        },
-    ];
-    //display questions
+        //display each question and answer set
+        $.each(qaChoice, function (i, v) {
+            const quiz = $("<div>");
+            const q = $("<p>").text("" + v.question + "").addClass("text-dark");
+            var a = $("<button>").text(v.answer[0]).addClass("btn btn-lg btn0 mx-2 mb-1 btn-dark");
+            var b = $("<button>").text(v.answer[1]).addClass("btn btn-lg btn1 mx-2 mb-1 btn-dark");
+            var c = $("<button>").text(v.answer[2]).addClass("btn btn-lg btn2 mx-2 mb-1 btn-dark");
+            var d = $("<button>").text(v.answer[3]).addClass("btn btn-lg btn3 mx-2 mb-1 btn-dark");
+            quiz.append(q).append(a).append(b).append(c).append(d);
+            console.log(quiz.html());
+            console.log("#qaPlace: " + $("#qaPlace").length);
 
-    qaChoice.sort(function () {
-        return 0.5 - Math.random();
+            $("#qaPlace").append(quiz);
+        });
+
+        //  $.each(qaChoice, function(_index, _val){
+        //      console.log(this.question);
+        //      console.log(this.answer); 
+        //  })
+
+        //score
+        const answerCorrect = 0;
+        const answerWrong = 0;
+
+        $("button").on("click", function (event) {
+            event.preventDefault;
+
+            if ($(this).text() === correctAnswer) {
+                answerCorrect++;
+                $("#results").html("<p> Correct: " + answerCorrect + "</p>");
+            
+            } else if($(this).text() != correctAnswer) {
+                answerWrong++;
+                $("#results").html("<p> Wrong: " + answerWrong + "</p>");
+                
+            };
+        })
+
+        //timer
+        var number = 10;
+        var intervalId;
+        function run() {
+            intervalId = setInterval(decrement, 1000);
+        }
+        function decrement() {
+            number--;
+            $("#timer").html("<h2>" + number + "</h2>");
+            if (number === 0) {
+                stop();
+            }
+        }
+        function stop() {
+            clearInterval(intervalId);
+        }
+        run();
     });
 
-    //display each question and answer set
-     $.each(qaChoice, function (i, v) {
-         console.log("i: " + i + " v: " + v);
-         const quiz = $("<div>");
-         const q = $("<p>").text("" + v.question + "").addClass("text-dark");
-         var a = $("<button>").text(v.answer[0]).addClass("btn btn-lg btn0 mx-2 btn-dark");
-         var b = $("<button>").text(v.answer[1]).addClass("btn btn-lg btn1 mx-2 btn-dark");
-         var c = $("<button>").text(v.answer[2]).addClass("btn btn-lg btn2 mx-2 btn-dark");
-         var d = $("<button>").text(v.answer[3]).addClass("btn btn-lg btn3 mx-2 btn-dark");
-         quiz.append(q).append(a).append(b).append(c).append(d);
-         console.log(quiz.html());
-         console.log("#qaPlace: " + $("#qaPlace").length);
-
-         $("#qaPlace").append(quiz);
-    });
-
-     $.each(qaChoice, function(_index, _val){
-         console.log(this.question);
-         console.log(this.answer); 
-     })
-    
-        
-
-    // $("button").on("click", function({
-
-    // });
-})
-
-var number = 100;
-
-    var intervalId;
-
-    function run() {
-    intervalId = setInterval(decrement, 1000);
-    }
-
-    function decrement() {
-      number--;
-      $("#timer").html("<h2>" + number + "</h2>");
-      if (number === 0) {
-        stop();
-      }
-    }
-
-    function stop() {
-      clearInterval(intervalId);
-    }
-
-    run();
-
-  
-    // $("#answer").on("click", "button", function(){
-    //     if ($(this).text()===) {
-    //         $(".qaPlace").html("<div class='qaPlace col-12'><img src='qaChoice[currentQIndex]image'></div>");
-    //         answerCorrect++;
-    //         //10 sec
-    //     } else {
-    //         $(".qaPlace").html("<div class='qaPlace col-12'><img src='qaChoice[currentQIndex]image'></div>");
-    //         answerWrong++;
-    //     };
-
-});//end document ready3
+}); //end document ready3
